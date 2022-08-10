840a989eea49955a00496d48e53988d759b29533
idrees <eeskdr5@gmail.com>
2022-08-10 23:55:43 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'pages/home.dart';
+import 'pages/loading.dart';
+import 'pages/location.dart';
+
+
+Future<void> main() async {
+  runApp( MaterialApp(
+    initialRoute: 'home',
+    routes: {
+      'loading' : (context) => Loading(),
+      'home': (context) => Home(),
+      'location' : (context) => ChooseLocation(),
+
+
+    },
+
+  ));
+}
+
+
